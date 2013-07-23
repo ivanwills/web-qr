f2e2b18cd9cfa81c3c3600f03980659a4d702369
Ivan Wills <ivan.wills@gmail.com>
2013-07-24 07:00:25 +1000

    Tidying up client and server

--- client/web.js
+++ client/web.js
@@ -7,119 +7,109 @@ Tables.qr.find().forEach(function(table) {
     Tables[table.collection] = new Meteor.Collection(table.collection);
 });
 
-if (Meteor.isClient) {
-
-    Template.nav.tables = function () {
-        // Read the collections specified in qr
-        Tables.qr.find({}, {}).forEach(function(table) {
-            var coll = table.collection;
-            if (!Tables[coll]) Tables[coll] = new Meteor.Collection(coll);
-        });
-
-        return Tables.qr.find({}, { sort : { pos : 1 } });
-    };
-
-    Template.sections.selected = function() {
-        if ( !Session.get('selected_table') ) {
-            Session.set('selected_table', this.collection);
-        }
-        return Session.equals("selected_table", this.collection) ? "active" : '';
-    };
-
-    Template.sections.table = function() {
-        return this.collection;
-    };
-
-    Template.sections.events({
-        'click' : function() {
-            Session.set("selected_table", this.collection);
-            Session.set("selected_label", null);
-        }
+Template.nav.tables = function () {
+    // Read the collections specified in qr
+    Tables.qr.find({}, {}).forEach(function(table) {
+        var coll = table.collection;
+        if (!Tables[coll]) Tables[coll] = new Meteor.Collection(coll);
     });
 
-    Template.sidebar.labels = function(a) {
-        var collection = Session.get('selected_table');
-        if ( !Tables || !collection )
-            return;
+    return Tables.qr.find({}, { sort : { pos : 1 } });
+};
 
-        var data = Tables.qr.find( { collection : collection } );
-        data.forEach(function(d) { data = d });
-        return data.sections;
-    };
+Template.sections.selected = function() {
+    if ( !Session.get('selected_table') ) {
+        Session.set('selected_table', this.collection);
+    }
+    return Session.equals("selected_table", this.collection) ? "active" : '';
+};
 
-    Template.label.selected = function() {
-        if ( !Session.get('selected_label') ) {
-            Session.set('selected_label', this.name);
-        }
-        return Session.equals("selected_label", this.name) ? "active" : '';
-    };
+Template.sections.table = function() {
+    return this.collection;
+};
 
-    Template.label.events({
-        'click' : function() {
-            Session.set("selected_label", this.name);
-        }
-    });
+Template.sections.events({
+    'click' : function() {
+        Session.set("selected_table", this.collection);
+        Session.set("selected_label", null);
+    }
+});
 
-    Template.data.data_heads = function() {
-        if ( !Tables || !Session.get('selected_table') || !Tables[Session.get('selected_table')])
-            return;
+Template.sidebar.labels = function(a) {
+    var collection = Session.get('selected_table');
+    if ( !Tables || !collection )
+        return;
 
-        var found = Tables.qr.findOne({ collection : Session.get('selected_table') });
+    var data = Tables.qr.find( { collection : collection } );
+    data.forEach(function(d) { data = d });
+    return data.sections;
+};
 
-        console.log('found ');
-        if (found) {
-            found = _.find(
-                found.sections,
-                function(section) {
-                    return Session.equals('selected_label', section.name)
-                }
-            );
-            if (!found) return;
+Template.label.selected = function() {
+    if ( !Session.get('selected_label') ) {
+        Session.set('selected_label', this.name);
+    }
+    return Session.equals("selected_label", this.name) ? "active" : '';
+};
 
-            console.log( 'headings ', found);
-            Session.set('labels', found.labels);
-            return found.labels;
-        }
-    };
+Template.label.events({
+    'click' : function() {
+        Session.set("selected_label", this.name);
+    }
+});
 
-    Template.data.values = function() {
-        var selected_table = Session.get('selected_table');
-        var selected_label = Session.get('selected_label');
-        console.log('table ', selected_table, ' - ', selected_label);
+Template.data.data_heads = function() {
+    if ( !Tables || !Session.get('selected_table') || !Tables[Session.get('selected_table')])
+        return;
 
-        if ( !Tables || !selected_table || !Tables[selected_table])
-            return;
+    var found = Tables.qr.findOne({ collection : Session.get('selected_table') });
 
-        console.log('finding ', selected_table, ' - ', selected_label);
-        var found = Tables[selected_table]
-            .find({ type : selected_label}, {limit : 299} );
+    console.log('found ');
+    if (found) {
+        found = _.find(
+            found.sections,
+            function(section) {
+                return Session.equals('selected_label', section.name)
+            }
+        );
+        if (!found) return;
 
-        if (found) {
-            console.log(selected_table + ' found values : ', found);
-            return found;
-        }
-        console.log('nothing found for ', selected_table, ' - ', selected_label);
-    };
+        console.log( 'headings ', found);
+        Session.set('labels', found.labels);
+        return found.labels;
+    }
+};
 
-    Template.value.value_cols = function() {
-        if ( !Session.get('selected_table') ) return;
+Template.data.values = function() {
+    var selected_table = Session.get('selected_table');
+    var selected_label = Session.get('selected_label');
+    console.log('table ', selected_table, ' - ', selected_label);
 
-        var columns = Session.get('labels');
-        var values = [];
-        for ( var i in columns ) {
-            values.push( this[ columns[i].name ] );
-        }
-        return values;
-    };
+    if ( !Tables || !selected_table || !Tables[selected_table])
+        return;
 
-    Template.col.attribute = function() {
-        console.log('attribute ', this);
-    };
+    console.log('finding ', selected_table, ' - ', selected_label);
+    var found = Tables[selected_table]
+        .find({ type : selected_label}, {limit : 299} );
 
-}
+    if (found) {
+        console.log(selected_table + ' found values : ', found);
+        return found;
+    }
+    console.log('nothing found for ', selected_table, ' - ', selected_label);
+};
 
-if (Meteor.isServer) {
-for (var key in Tables) console.log(key);
-    Meteor.startup(function () {
-    });
-}
+Template.value.value_cols = function() {
+    if ( !Session.get('selected_table') ) return;
+
+    var columns = Session.get('labels');
+    var values = [];
+    for ( var i in columns ) {
+        values.push( this[ columns[i].name ] );
+    }
+    return values;
+};
+
+Template.col.attribute = function() {
+    console.log('attribute ', this);
+};
