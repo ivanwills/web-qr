f369eb76345fddfcd057033185ce92cbfea727c2
Ivan Wills <ivan.wills@gmail.com>
2013-07-31 20:42:37 +1000

    Cleaned out log messages

--- client/routes.js
+++ client/routes.js
@@ -1,13 +1,11 @@
 
 Meteor.Router.add({
     '/' : function() {
-        console.log('root');
         if ( !Session.get('qr') ||!Tables || !Tables.qr || !Tables.qr.find().count() ) return 'blank';
         setTimeout(function(){ Meteor.Router.to('/' + Session.get('selected_table') ) }, 100);
         return 'loading';
     },
     '/:table' : function(table) {
-        console.log('table');
         var current_table = Session.get('selected_table');
         if ( current_table != table ) {
             Session.set('selected_table', table);
@@ -16,7 +14,6 @@ Meteor.Router.add({
         return 'qr';
     },
     '/:table/:label' : function(table, label) {
-        console.log('label');
         var current_table = Session.get('selected_table');
         var current_label = Session.get('selected_label');
         if ( current_table != table ) {
@@ -51,7 +48,6 @@ Deps.autorun(function () {
         });
         Tables[table] = new Meteor.Collection(table);;
     }
-    console.log(Tables);
     Session.set('qr', true);
 });
 
