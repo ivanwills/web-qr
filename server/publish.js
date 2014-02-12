4c47c3632964892edceb9de94993fbe1ebe42eec
Ivan Wills <ivan.wills@gmail.com>
2014-02-12 16:47:40 +1100

    Removed common code (put wholly into server side)

--- server/publish.js
+++ server/publish.js
@@ -1,4 +1,22 @@
 
+Tables = {
+    qr : new Meteor.Collection("qr")
+};
+Tables.qr.find().forEach(function(table) {
+    // only executed on server initally
+    Tables[table.collection] = new Meteor.Collection(table.collection);
+
+    console.log(table.collection, ' = ', Tables[table.collection].find().count());
+    Tables[table.collection].find().forEach(function(table) {
+        if (!table.collection)
+            return;
+
+        console.log(table.collection);
+        Tables[table.collection] = new Meteor.Collection(table.collection);
+    });
+    console.info('Tables = ', Object.keys(Tables).length);
+});
+
 Meteor.startup(function () {
     console.info('------ Tables = ', Object.keys(Tables).length);
     for ( var table in Tables ) {
