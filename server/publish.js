191e626aa4b2c638a652de6ff6d053d036b0e56f
Ivan Wills <Ivan.Wills@optus.com.au>
2014-02-18 07:58:43 +1100

    More jshint cleanups

--- server/publish.js
+++ server/publish.js
@@ -1,3 +1,4 @@
+/*global Tables,Meteor,console */
 
 Tables = {
     qr : new Meteor.Collection("qr")
@@ -20,14 +21,16 @@ Tables.qr.find().forEach(function(table) {
 Meteor.startup(function () {
     console.info('------ Tables = ', Object.keys(Tables).length);
     for ( var table in Tables ) {
-        console.log(1, table);
-        (function(table) {
-        console.log(2, table);
-            Meteor.publish(table, function() {
-                console.log(3, table, Tables[table].find().count());
-                return Tables[table].find();
-            });
-        })(table);
+        if ( Tables.hasOwnProperty(table) ) {
+            console.log(1, table);
+            (function(table) {
+                console.log(2, table);
+                Meteor.publish(table, function() {
+                    console.log(3, table, Tables[table].find().count());
+                    return Tables[table].find();
+                });
+            })(table);
+        }
     }
 
 });
